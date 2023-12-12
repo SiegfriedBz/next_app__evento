4a24394a660397b36b8672fb162c4ffb75d87786
siegfriedbz <siegfried.bozza@yahoo.com>
2023-12-12 09:16:55 +0100

    clean up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
-  console.log("request.url", request.url)
   return NextResponse.redirect(new URL("/events/all", request.url))
 }
 
