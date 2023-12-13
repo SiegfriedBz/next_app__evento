76e7883bb20b0be2b731a8946cdabe65e558b8bd
siegfriedbz <siegfried.bozza@yahoo.com>
2023-12-13 08:08:07 +0100

    fix /event/[slug] og image

--- src/app/event/[slug]/opengraph-image.tsx
+++ src/app/event/[slug]/opengraph-image.tsx
@@ -1,8 +1,11 @@
 import { ImageResponse } from "next/og"
 import { capitalize } from "../../../lib/utils"
+import { getEvent } from "@/lib/server-utils"
+import { EventoEvent } from "@prisma/client"
+import Image from "next/image"
 
 // Image metadata
-export const alt = "Evento"
+export const alt = "Evento event"
 export const size = {
   width: 1200,
   height: 630,
@@ -10,12 +13,29 @@ export const size = {
 
 export const contentType = "image/png"
 
-export default async function Image({ params }: { params: { slug: string } }) {
+export default async function OgImage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  let eventItem: EventoEvent | undefined = undefined
+
+  try {
+    eventItem = await getEvent(params.slug)
+  } catch (e) {
+    console.log(e)
+  }
+
   return new ImageResponse(
     (
-      <section>
-        <h1>{capitalize(params.slug)}</h1>
-        <p>Evento - Browse events around you</p>
+      <section className='relative'>
+        {eventItem !== undefined && (
+          <Image src={eventItem.imageUrl} {...size} alt={alt} />
+        )}
+        <div className='absolute bottom-2 right-2 z-50 text-white/90'>
+          <h1 className='h1'>{capitalize(params.slug)}</h1>
+          <p>Evento - Browse events around you</p>
+        </div>
       </section>
     )
   )
